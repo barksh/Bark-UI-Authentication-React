e4fe30baf677efab108abb5760e800d73a80275f
wmxpy <wm@mengw.io>
2023-02-25 20:47:08 -0600

    add base configs

---
+++ test/unit/placeholder.test.ts
@@ -0,0 +1,19 @@
+/**
+ * @author WMXPY
+ * @namespace Placeholder
+ * @description Placeholder
+ * @override Unit Test
+ */
+
+import { expect } from "chai";
+import * as Chance from "chance";
+
+describe('Placeholder', (): void => {
+
+    const chance: Chance.Chance = new Chance('placeholder');
+
+    it('Placeholder', (): void => {
+
+        expect(chance.string()).to.be.not.equal(chance.string());
+    });
+});
