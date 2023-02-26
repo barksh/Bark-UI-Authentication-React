9143cf20c9aa27d6cf3e9c7f3062bb5f9e2494f1
wmxpy <wm@mengw.io>
2023-02-25 20:51:29 -0600

    add typescirpt configs

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+/**
+ * @author WMXPY
+ * @namespace BuilderHub
+ * @description Main
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement,
+).render(<React.StrictMode>
+    <div>123</div>
+</React.StrictMode>);
