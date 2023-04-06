40a90f38208cbeddcdf96a1a2aee4893a693c924
wmxpy <wm@mengw.io>
2023-04-05 22:14:17 -0500

    implement exposure key page

--- src/i18n/profile/en-us.ts
+++ src/i18n/profile/en-us.ts
@@ -12,6 +12,7 @@ export const ENGLISH_UNITED_STATES: Record<PROFILE, string> = {
     [PROFILE.ERROR_NO_EXPOSURE_KEY_DESCRIPTION]: "This page encountered an error, if you are redirected to this page, please try again or contact the administrator of the application you are attempt to sign-in.",
 
     [PROFILE.MANUAL_INPUT_EXPOSURE_KEY]: "Manual Input Exposure Key",
+    [PROFILE.MANUAL_INPUT_EXPOSURE_KEY_DESCRIPTION]: "Please input the exposure key manually, if you are not sure about the exposure key, please try again or contact the administrator of the application you are attempt to sign-in.",
 
     [PROFILE.SUBMITTED_SUCCESSFULLY]: "Submitted Successfully",
     [PROFILE.SUBMITTED_SUCCESSFULLY_DESCRIPTION]: "Your sign-in has been successfully submitted, you can safely close this page and revisit the application you are signing-in.",
--- src/routes/exposure-key.tsx
+++ src/routes/exposure-key.tsx
@@ -4,11 +4,19 @@
  * @description Exposure Key
  */
 
-import { Button, Card, CenteredLayout, InputText } from "@barksh/bark-design-react";
+import { PostTouchV1ProxyResponse } from "@barksh/authentication-types";
+import { Button, Card, CenteredLayout, InputText, Spacing } from "@barksh/bark-design-react";
+import { postTouchV1Proxy } from "@barksh/client-authenticator-browser";
+import { SudooFormat } from "@sudoo/internationalization";
 import * as React from "react";
-import { MdLockOpen } from "react-icons/md";
+import { MdOutbound } from "react-icons/md";
+import { useFormat } from "../i18n/hook";
+import { PROFILE } from "../i18n/profile/profile";
+import { EnvironmentVariables } from "../util/environment";
 
 export const ExposureKeyView: React.FC = () => {
+
+    const format: SudooFormat<PROFILE> = useFormat();
 
     const [loading, setLoading] = React.useState(false);
 
@@ -17,16 +25,25 @@ export const ExposureKeyView: React.FC = () => {
     const submitAction = async (): Promise<void> => {
 
         setLoading(true);
+
+        const touchResult: PostTouchV1ProxyResponse = await postTouchV1Proxy(
+            EnvironmentVariables.moduleAuthenticationHost,
+            {
+                exposureKey,
+            },
+        );
+
+        console.log(touchResult);
     };
 
     return (<CenteredLayout>
         <Card
             size="large"
-            headerTitle="Exposure Key"
+            headerTitle={format.get(PROFILE.MANUAL_INPUT_EXPOSURE_KEY)}
             minWidth="min(512px, 100vw)"
             maxWidth="768px"
             actions={<Button
-                prefix={<MdLockOpen
+                prefix={<MdOutbound
                     size={24}
                 />}
                 onClick={() => {
@@ -36,6 +53,10 @@ export const ExposureKeyView: React.FC = () => {
                 Submit
             </Button>}
         >
+            {format.get(PROFILE.MANUAL_INPUT_EXPOSURE_KEY_DESCRIPTION)}
+            <Spacing
+                size="large"
+            />
             <InputText
                 title="Exposure Key"
                 placeholder="Exposure Key"
