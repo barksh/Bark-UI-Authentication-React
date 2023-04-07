980fe3249d6c3396ce2f3b00488c059267d6f7d6
wmxpy <wm@mengw.io>
2023-04-06 20:58:18 -0500

    add i18n for more pages

--- src/i18n/profile/en-us.ts
+++ src/i18n/profile/en-us.ts
@@ -13,6 +13,9 @@ export const ENGLISH_UNITED_STATES: Record<PROFILE, string> = {
 
     [PROFILE.MANUAL_INPUT_EXPOSURE_KEY]: "Manual Input Exposure Key",
     [PROFILE.MANUAL_INPUT_EXPOSURE_KEY_DESCRIPTION]: "Please input the exposure key manually, if you are not sure about the exposure key, please try again or contact the administrator of the application you are attempt to sign-in.",
+    [PROFILE.MANUAL_INPUT_EXPOSURE_KEY_ERROR]: "Exposure key verification failed",
+
+    [PROFILE.SIGN_IN_FAILED_DESCRIPTION]: "Sign-in failed, please try again.",
 
     [PROFILE.SUBMITTED_SUCCESSFULLY]: "Submitted Successfully",
     [PROFILE.SUBMITTED_SUCCESSFULLY_DESCRIPTION]: "Your sign-in has been successfully submitted, you can safely close this page and revisit the application you are signing-in.",
